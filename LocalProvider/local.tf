6e12707c3a28048a98f0e2de660d4c39c671b2aa
Ziya <ziya.cse@gmail.com>
2021-06-20 15:29:20 +0530

    updated the comments

--- LocalProvider/local.tf
+++ LocalProvider/local.tf
@@ -8,6 +8,8 @@ resource "local_file" "test"{
 
 
 // Adding sensitive content so that it doesn't show output on the terminal and giving perimission to the file
+// We cannot use content and sensitive content in the same resource
+// directory_permission is the other argument left in the local_file provider
 
 resource "local_file" "sensitive" {
     filename = "./Outputs/sensitive_co.txt"
