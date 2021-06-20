223b6de14b4768e792e78d5e12bc8872b9e0a1f4
Ziya <ziya.cse@gmail.com>
2021-06-20 08:51:08 +0530

    first terraform code

---
+++ LocalProvider/local.tf
@@ -0,0 +1,16 @@
+/* The below resource creates a local.txt file with the content as per the argument
+Resource type used here is local_file */
+
+resource "local_file" "test"{
+    filename = "./Outputs/local.txt"
+    content = "Hey, Welcome to the resource type local_File!"
+}
+
+
+// Adding sensitive content so that it doesn't show output on the terminal and giving perimission to the file
+
+resource "local_file" "sensitive" {
+    filename = "./Outputs/sensitive_co.txt"
+    file_permission = "0755"
+    sensitive_content = "Hey, Welcome to the sensitive content!"
+}
