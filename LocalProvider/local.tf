2969c5d0033fcbdbc847ad1b64b8aceb236ff832
Ziya <ziya.cse@gmail.com>
2021-06-20 17:51:34 +0530

    updated code by using multi providers

--- LocalProvider/local.tf
+++ LocalProvider/local.tf
@@ -1,9 +1,9 @@
 /* The below resource creates a local.txt file with the content as per the argument
 Resource type used here is local_file */
 
-resource "local_file" "test"{
-    filename = "./Outputs/local.txt"
-    content = "Hey, Welcome to the resource type local_File!"
+resource "local_file" "test" {
+  filename = "./Outputs/local.txt"
+  content  = "Hey, Welcome to the resource type local_File!"
 }
 
 
@@ -12,7 +12,25 @@ resource "local_file" "test"{
 // directory_permission is the other argument left in the local_file provider
 
 resource "local_file" "sensitive" {
-    filename = "./Outputs/sensitive_co.txt"
-    file_permission = "0755"
-    sensitive_content = "Hey, Welcome to the sensitive content!"
+  filename          = "./Outputs/sensitive_co.txt"
+  file_permission   = "0755"
+  sensitive_content = "Hey, Welcome to the sensitive content!"
+}
+
+// Adding another provider to verify that Terraform supports multi-provider
+
+resource "random_pet" "pet_name" {
+  length    = "2"
+  separator = "."
+  prefix    = "Mrs"
+}
+
+output "NameOfThePet" {
+  value = random_pet.pet_name.id
+}
+// Adding the above result (Pet name) as content in the next resource
+
+resource "local_file" "Create_file" {
+  filename = "./Outputs/Multi.txt"
+  content  = random_pet.pet_name.id
 }
