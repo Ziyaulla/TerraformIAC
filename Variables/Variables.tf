8240e116a5e550e13f21f0d8088ffb298832d909
Ziya <ziya.cse@gmail.com>
2021-06-23 17:10:46 +0530

    Variable insights

---
+++ Variables/Variables.tf
@@ -0,0 +1,38 @@
+variable "name"{
+    type = string
+    default = "Ziya"
+}
+
+variable "filename" {
+  type = map
+  default = {
+      filename = "./Outputs/sampleOutput.txt"
+  }
+}
+
+variable "number1" {
+  type = number
+  default = 5
+}
+
+variable "StringT" {
+  default = ""
+}
+
+variable "filelist" {
+  type = map
+  default = {
+    file1 = "./Outputs/file1.txt"
+    file2 = "./Outputs/file2.txt"
+  }
+}
+
+variable "True_False" {
+  type = bool
+  default = true
+}
+
+variable "StringSet" {
+  type = list(string)
+  default = [ "Ziya","Dhoni","Smiling" ]
+}
