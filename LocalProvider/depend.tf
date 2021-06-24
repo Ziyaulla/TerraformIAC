988385eb919d78976ed7b88d319f9baabd50104b
Ziya <ziya.cse@gmail.com>
2021-06-24 20:19:05 +0530

    updated

--- LocalProvider/depend.tf
+++ LocalProvider/depend.tf
@@ -4,13 +4,16 @@ resource "local_file" "dependency" {
   filename = "./Outputs/file1.txt"
   content = "This file depends on the file2.txt"
   depends_on = [
-    time_sleep.sleepT
+    local_file.dependency2
   ]
 }
 
 resource "local_file" "dependency2" {
   filename = "./Outputs/file2.txt"
   content = "This is the first file created at ${time_static.timeNow.id}"
+  depends_on = [
+    time_sleep.sleepT
+  ]
 }
 
 resource "time_sleep" "sleepT" {
