72cb46f4289f13cfbb6c8a84da9ecb99867c464d
Ziya <ziya.cse@gmail.com>
2021-06-27 21:10:47 +0530

    updated random code

--- module-samples/main.tf
+++ module-samples/main.tf
@@ -3,7 +3,15 @@ module "sample_module" {
   fname = "module3.txt"
 }
 
+module "fileExtend" {
+  source = "./local_m2"
+  fname = "module4.txt"
+}
 
 output "filePerm" {
   value = module.sample_module.name
+}
+
+output "filePerm2" {
+  value = module.fileExtend.name
 }
